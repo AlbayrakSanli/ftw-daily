1bc4d960dd850e21fd6ccc04d6b40bb71d2e6b04
Vesa Luusua <vesa.luusua@gmail.com>
2020-06-02 02:35:29 +0300

    Update filter and sort configurations

--- src/marketplace-custom-config.js
+++ src/marketplace-custom-config.js
@@ -1,69 +1,131 @@
 /*
  * Marketplace specific configuration.
+ *
+ * Every filter needs to have following keys:
+ * - **id**: unique id of the filter
+ * - **label**: The default label of the filter
+ * - **type**: string that represents one of the existing filter components
+ * - **group**: is this 'primary' or 'secondary' filter?
+ * - **queryParamNames**: describes parameters to be used with API (e.g. 'price' or 'pub_amenities')
+ * - **config** extra configuration that the filter component needs
+ *
+ * Note 1: Labels could be tied to translation file by importing FormattedMessage:
+ *         <FormattedMessage id="some.translation.key.here" />
+ *
+ * Note 2: If you need to add new custom filter components,
+ *         you need to take those into use in:
+ *         src/containers/SearchPage/FilterComponent.js
+ *
+ * Note 3: If you just want to create more enum filters
+ *         (i.e. SelectSingleFilter, SelectMultipleFilter),
+ *         you can just add more configurations with those filter types.
  */
 
-export const amenities = [
+export const filters = [
   {
-    key: 'towels',
-    label: 'Towels',
+    id: 'dates',
+    label: 'Dates',
+    type: 'BookingDateRangeFilter',
+    group: 'primary',
+    queryParamNames: ['dates'],
+    config: {},
   },
   {
-    key: 'bathroom',
-    label: 'Bathroom',
+    id: 'price',
+    label: 'Price',
+    type: 'PriceFilter',
+    group: 'primary',
+    queryParamNames: ['price'],
+    // Price filter configuration
+    // Note: unlike most prices this is not handled in subunits
+    config: {
+      min: 0,
+      max: 1000,
+      step: 5,
+    },
   },
   {
-    key: 'swimming_pool',
-    label: 'Swimming pool',
+    id: 'keyword',
+    label: 'Keyword',
+    type: 'KeywordFilter',
+    group: 'primary',
+    queryParamNames: ['keywords'],
+    // NOTE: If you are ordering search results by distance
+    // the keyword search can't be used at the same time.
+    // You can turn on/off ordering by distance from config.js file.
+    config: {},
   },
   {
-    key: 'own_drinks',
-    label: 'Own drinks allowed',
+    id: 'category',
+    label: 'Category',
+    type: 'SelectSingleFilter',
+    group: 'secondary',
+    queryParamNames: ['pub_category'],
+    config: {
+      // "key" is the option you see in Flex Console.
+      // "label" is set here for the UI only.
+      // Note: label is not added through the translation files
+      // to make filter customizations a bit easier.
+      options: [
+        { key: 'smoke', label: 'Smoke' },
+        { key: 'electric', label: 'Electric' },
+        { key: 'wood', label: 'Wood' },
+        { key: 'other', label: 'Other' },
+      ],
+    },
   },
   {
-    key: 'jacuzzi',
-    label: 'Jacuzzi',
-  },
-  {
-    key: 'audiovisual_entertainment',
-    label: 'Audiovisual entertainment',
-  },
-  {
-    key: 'barbeque',
-    label: 'Barbeque',
-  },
-  {
-    key: 'own_food_allowed',
-    label: 'Own food allowed',
+    id: 'amenities',
+    label: 'Amenities',
+    type: 'SelectMultipleFilter',
+    group: 'secondary',
+    queryParamNames: ['pub_amenities'],
+    config: {
+      // Optional modes: 'has_all', 'has_any'
+      // https://www.sharetribe.com/api-reference/marketplace.html#extended-data-filtering
+      searchMode: 'has_all',
+
+      // "key" is the option you see in Flex Console.
+      // "label" is set here for this web app's UI only.
+      // Note: label is not added through the translation files
+      // to make filter customizations a bit easier.
+      options: [
+        {
+          key: 'towels',
+          label: 'Towels',
+        },
+        {
+          key: 'bathroom',
+          label: 'Bathroom',
+        },
+        {
+          key: 'swimming_pool',
+          label: 'Swimming pool',
+        },
+        {
+          key: 'own_drinks',
+          label: 'Own drinks allowed',
+        },
+        {
+          key: 'jacuzzi',
+          label: 'Jacuzzi',
+        },
+        {
+          key: 'audiovisual_entertainment',
+          label: 'Audiovisual entertainment',
+        },
+        {
+          key: 'barbeque',
+          label: 'Barbeque',
+        },
+        {
+          key: 'own_food_allowed',
+          label: 'Own food allowed',
+        },
+      ],
+    },
   },
 ];
-
-export const categories = [
-  { key: 'smoke', label: 'Smoke' },
-  { key: 'electric', label: 'Electric' },
-  { key: 'wood', label: 'Wood' },
-  { key: 'other', label: 'Other' },
-];
-
-// Price filter configuration
-// Note: unlike most prices this is not handled in subunits
-export const priceFilterConfig = {
-  min: 0,
-  max: 1000,
-  step: 5,
-};
-
-// Activate booking dates filter on search page
-export const dateRangeFilterConfig = {
-  active: true,
-};
-
-// Activate keyword filter on search page
-
-// NOTE: If you are ordering search results by distance the keyword search can't be used at the same time.
-// You can turn off ordering by distance in config.js file
-export const keywordFilterConfig = {
-  active: true,
-};
 
 export const sortConfig = {
   // Enable/disable the sorting control in the SearchPage
@@ -71,6 +133,10 @@ export const sortConfig = {
 
   // Internal key for the relevance option, see notes below.
   relevanceKey: 'relevance',
+
+  // Keyword filter is sorting the results already by relevance.
+  // If keyword filter is active, we need to disable sorting.
+  conflictingFilters: ['keyword'],
 
   options: [
     { key: 'createdAt', label: 'Newest' },
